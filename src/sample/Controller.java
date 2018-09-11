2e4de45e63e38b953f1782596094af60352a852b
Kamp Duong <kodingkamp@gmail.com>
2018-09-11 13:21:47 -0400

    Added Mac-friendly GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -93,7 +94,7 @@ private static String team;
 
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-    stage.setScene(new Scene(root,744,506));
+    stage.setScene(new Scene(root,287,372));
     stage.show();
 
   }
@@ -123,17 +124,18 @@ private static String team;
 
     Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-    stage.setScene(new Scene(root,744,506));
+    stage.setScene(new Scene(root,287,372));
     stage.show();
 
   }
+
   @FXML
   void createActClicked(ActionEvent event) throws  IOException{
     Stage stage = Main.getPrimaryStage();
 
     Parent root = FXMLLoader.load(getClass().getResource("CreateAccount.fxml"));
 
-    stage.setScene(new Scene(root,600,400));
+    stage.setScene(new Scene(root,287,372));
     stage.show();
   }
   @FXML
@@ -158,8 +160,21 @@ private static String team;
     System.out.println("The Calendar tab is selected");
   }
 
+  @FXML
+  void closeProgram(){
+    Platform.exit();
+  }
 
+  @FXML
+  void minimizeProg(){
+    Stage stage = Main.getPrimaryStage();
+    stage.setIconified(true);
+  }
 
-
+  @FXML
+  void maxProgWindow(){
+    Stage stage = Main.getPrimaryStage();
+    stage.setMaximized(true);
+  }
 
 }
