2a620930175570a3411e9454d24aedd0b2cd8eeb
brandonschultz <baschultz2842@eagle.fgcu.edu>
2018-10-16 21:25:04 -0400

    Fixed design and layout, changed choice box to store account types instead of teams

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,185 +1,143 @@
 package sample;
 
+
+import java.awt.event.ActionEvent;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javax.xml.soap.Text;
+import javafx.stage.Stage;
 
 public class Controller {
+  private static String name;
+  private static String userType;
 
-private static String name;
-private static String team;
-
-  ObservableList<String> teamStatus = FXCollections.observableArrayList("Team 1",
-      "Team 2","Team 3");
+  ObservableList<String> accountTypeList = FXCollections.observableArrayList("Fan","Player","Manager");
 
   @FXML
-  private TextField nameInput;
-  @FXML
-  private TextField passInput;
-  @FXML
-  private TextField usernameCreate;
-  @FXML
-  private TextField passCreate;
-  @FXML
-  private ChoiceBox teamChoiceBox;
-  @FXML
-  private Label welcomeLabel;
-
-  //bhjbjjhfgdh
-
-
-
+  private TextField createUsername;
 
   @FXML
-  void logInPressed(ActionEvent event) throws IOException {
-    //System.out.println("YOOOOO");
-    String user = nameInput.getText();
-    String pass = passInput.getText();
-    this.name = user;
+  private PasswordField createPassword;
+
+  @FXML
+  private Button backButton;
+
+  @FXML
+  private Button createButton;
+  @FXML
+  private TextField usernameEntered;
+  @FXML
+  private TextField passwordEntered;
+  @FXML
+  private ChoiceBox accountTypeChoice;
 
 
-    //Read from file
-    try{
-      FileReader fr = new FileReader("Accounts.txt");
-      BufferedReader br = new BufferedReader(fr);
-      String str;
-      boolean found = false;
-      while((str = br.readLine()) != null && !found){
-        if((user.toLowerCase() + " " + pass).equals(str)){
+
+
+
+
+public void logInButtonPressed(){
+  String user = usernameEntered.getText();
+  String pass = passwordEntered.getText();
+  this.name = user;
+
+
+  //Read from file
+  try{
+    FileReader fr = new FileReader("Accounts.txt");
+    BufferedReader br = new BufferedReader(fr);
+    String str;
+    boolean found = false;
+    while((str = br.readLine()) != null && !found){
+      if((user.toLowerCase() + " " + pass).equals(str)){
 //          System.out.println("Logged In!!");
-          found = true;
-          this.team = br.readLine();
+        found = true;
 
-          Stage stage = Main.getPrimaryStage();
 
-          Parent root = FXMLLoader.load(getClass().getResource("LoggedIn.fxml"));
+        Stage stage = Main.getPrimaryStage();
 
-          stage.setScene(new Scene(root,600,400));
-          stage.show();
-        }
+        Parent root = FXMLLoader.load(getClass().getResource("LoggedIn.fxml"));
+
+        stage.setScene(new Scene(root,600,400));
+        stage.show();
       }
-      if(!found){
-        AlertBox.display("No Account","Could Not Find Account. Please"
-            + " Try Again or Create Account");
-        passInput.clear();
-      }
-      br.close();
-
-    }catch (IOException er){
-      System.out.println("error");
     }
-
-
-
-
-
-
-  }
-  @FXML
-  void logOutPressed(ActionEvent event) throws IOException{
-    Stage stage = Main.getPrimaryStage();
-
-    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-    stage.setScene(new Scene(root,287,372));
-    stage.show();
-
-  }
-  @FXML
-  void createClicked(ActionEvent event) throws IOException{
-    try{
-      FileWriter fw = new FileWriter("Accounts.txt",true);
-      PrintWriter pw = new PrintWriter(fw);
-
-      String user = usernameCreate.getText();
-      String pass = passCreate.getText();
-
-      pw.println(user.toLowerCase() + " " + pass.toLowerCase());
-      pw.println(teamChoiceBox.getValue());
-
-      //test = " " + (String)(teamChoiceBox.getValue());
-      pw.close();
-
-
-
-    }catch (IOException er){
-      System.out.println("ERROR");
+    if(!found){
+      AlertBox.display("No Account","Could Not Find Account. Please"
+          + " Try Again or Create Account");
+      passwordEntered.clear();
     }
-    AlertBox.display("Account created","Account Successfully Created. Click"
-        + " \'Ok\' to go sign in");
-    Stage stage = Main.getPrimaryStage();
+    br.close();
 
-    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-    stage.setScene(new Scene(root,287,372));
-    stage.show();
-
+  }catch (IOException er){
+    System.out.println("error");
   }
 
-  @FXML
-  void createActClicked(ActionEvent event) throws  IOException{
-    Stage stage = Main.getPrimaryStage();
+}
 
-    Parent root = FXMLLoader.load(getClass().getResource("CreateAccount.fxml"));
+@FXML
+  public void createAccountButtonPressed() throws IOException {
+  Stage stage = Main.getPrimaryStage();
 
-    stage.setScene(new Scene(root,287,372));
-    stage.show();
-  }
+  Parent root = FXMLLoader.load(getClass().getResource("CreateAccount.fxml"));
+
+  stage.setScene(new Scene(root,600,450));
+  stage.show();
+}
+public void backButtonPressed() throws IOException{
+  Stage stage = Main.getPrimaryStage();
+
+  Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+  stage.setScene(new Scene(root,600,450));
+  stage.show();
+}
+public void createButtonClicked() throws IOException{
+  try{
+    FileWriter fw = new FileWriter("Accounts.txt",true);
+    PrintWriter pw = new PrintWriter(fw);
+
+    String user = createUsername.getText();
+    String pass = createPassword.getText();
+
+    pw.println(user.toLowerCase() + " " + pass.toLowerCase());
+
+    pw.close();
+
+    userType = accountTypeChoice.getValue().toString();
+
+    System.out.println(userType);
 
 
+  }catch (IOException er){
+    System.out.println("ERROR");
+  }
+  AlertBox.display("Account created","Account Successfully Created. Click"
+      + " \'Ok\' to go sign in");
+  Stage stage = Main.getPrimaryStage();
 
-  @FXML
-  private void setTeamChoiceBox(){
-    teamChoiceBox.setItems(teamStatus);
-  }
-  @FXML
-  private void welcomeSelected(){
-    welcomeLabel.setText("Welcome, " + Controller.name + ", to " + Controller.team);
+  Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-  }
-  @FXML
-  private void signOutSelected(){
-    System.out.println("The sign out tab is selected");
-  }
-  @FXML
-  private void statsSelected(){
-    System.out.println("The Stats tab is selected");
-  }
-  @FXML
-  private void calSelected(){
-    System.out.println("The Calendar tab is selected");
-  }
+  stage.setScene(new Scene(root,600,450));
+  stage.show();
 
-  @FXML
-  void closeProgram(){
-    Platform.exit();
-  }
+}
+@FXML
+  public void setAccountTypeChoice(){
+  accountTypeChoice.setItems(accountTypeList);
+}
 
-  @FXML
-  void minimizeProg(){
-    Stage stage = Main.getPrimaryStage();
-    stage.setIconified(true);
-  }
-
-  @FXML
-  void maxProgWindow(){
-    Stage stage = Main.getPrimaryStage();
-    stage.setMaximized(true);
-  }
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-
 public class Main extends Application {
 
     private static Stage primaryStage;
@@ -16,26 +15,23 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         setPrimaryStage(primaryStage);
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-        primaryStage.setTitle("LogIn Page");
-        primaryStage.setScene(new Scene(root,287,372));
-
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle("Login Page");
+        primaryStage.setScene(new Scene(root, 600, 450));
         primaryStage.show();
     }
+
+
 
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    private void setPrimaryStage(Stage stage){
-
-        Main.primaryStage = stage;
+    private void setPrimaryStage(Stage primaryStage) {
+        Main.primaryStage = primaryStage;
     }
 
-    static public Stage getPrimaryStage(){
-
+    public static Stage getPrimaryStage(){
         return primaryStage;
     }
 }
