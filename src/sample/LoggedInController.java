42438fc04ab0ed87f7b7027e5b6a366b79c67d41
Kamp Duong <kodingkamp@gmail.com>
2018-11-23 19:13:55 -0500

    Dummy games and minor accounts management adjustment

--- src/sample/LoggedInController.java
+++ src/sample/LoggedInController.java
@@ -26,7 +26,6 @@ import javafx.geometry.VPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
-import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
